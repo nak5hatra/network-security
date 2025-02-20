64da03896b7db552bab5c8b06dc119e5ad535027
Nakshatra Sharma <nakshatrasharma786@gmail.com>
2025-02-20 22:53:42 +0530

    data transformation completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import TrainingPipelineConfig
@@ -9,20 +10,27 @@ from networksecurity.components.data_validation import DataValidation
 
 if __name__ == "__main__":
     try:
-        trainingpipelineconfig = TrainingPipelineConfig()
-        dataingestionconfig = DataIngestionConfig(trainning_pipeline_config=trainingpipelineconfig)
-        data_ingestion = DataIngestion(data_ingestion_config=dataingestionconfig)
+        training_pipeline_config = TrainingPipelineConfig()
+        data_ingestion_config = DataIngestionConfig(trainning_pipeline_config=training_pipeline_config)
+        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
         logging.info("initiate the data ingestion")
-        dataingestionartifact = data_ingestion.initiate_data_ingestion()
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data initiation completed")
-        print(dataingestionartifact)
+        print(data_ingestion_artifact)
         
-        datavalidationconfig = DataValidationConfig(training_pipeline_config=trainingpipelineconfig)
-        data_validation = DataValidation(data_ingestion_artifact=dataingestionartifact, data_validation_config=datavalidationconfig)
+        data_validation_config = DataValidationConfig(training_pipeline_config=training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact, data_validation_config=data_validation_config)
         logging.info("initiate the data validation")
         data_validation_artifact =  data_validation.initiate_data_validation()
         logging.info("Data validation completed")
         print(data_validation_artifact)
+        
+        data_transformation_config = DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+        data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact, data_transformation_config=data_transformation_config)
+        logging.info("inittate the data transformation")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("data transformation completed")
+        print(data_transformation_artifact)
         
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -8,7 +8,7 @@ import pandas as pd
 defining common constent variable for training pipeline.
 """
 
-TARGET_COLUM: str = "Result"
+TARGET_COLUMN: str = "Result"
 PIPELINE_NAME: str = "NetworkSecurity"
 ARTIFACT_DIR: str = "Artifacts"
 FILE_NAME: str = "phisingData.csv"
@@ -17,6 +17,7 @@ TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
 SCHEMA_FILE_PATH: str = os.path.join("data_schema", "schema.yaml")
+PREPROCESSING_OBJECT_FILE_NAME: str = "preprocessing.pickle"
 
 """
 Data Ingestion related constant start with DATA_INGESTION var name.
@@ -38,3 +39,19 @@ DATA_VALIDATION_VALID_DIR: str = "valid"
 DATA_VALIDATION_INVALID_DIR: str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+
+"""
+Data Transformation related constent start with DATA_TRANSFORMATION var name.
+"""
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+
+## kkn imputer to replace nan values
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform",
+}
